72b56d5dbafb0104ee1badb6d481d873c37a23a1
Niklas Holmgren <niklas.holmgren@mindset.se>
2010-07-06 23:44:11 +0200

    Added a caching layer to the ArtworkLoader.

--- ArtworkLoader.m
+++ ArtworkLoader.m
@@ -10,14 +10,43 @@
 #import "Release.h"
 
 
+#define cachePathForKey(key) [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:[NSString stringWithFormat:@"Artwork/Cache/%@", [key stringByReplacingOccurrencesOfString:@"/" withString:@"|"]]]
+
+
 @implementation ArtworkLoader
 
 @synthesize url, delegate;
 
--(void)main {
+- (BOOL)hasCacheForKey:(NSString*)key {
+	return [[NSFileManager defaultManager] fileExistsAtPath:cachePathForKey(key)];
+}
+
+- (void)setCachedData:(NSData *)data forKey:(NSString *)key {
+	[[NSFileManager defaultManager] createDirectoryAtPath:cachePathForKey(@"") 
+							  withIntermediateDirectories:YES 
+											   attributes:nil 
+													error:NULL];
+	[data writeToFile:cachePathForKey(key) atomically:YES];
+}
+
+- (NSData *)getCachedData:(NSString *)key {
+	return [NSData dataWithContentsOfFile:cachePathForKey(key) options:0 error:NULL];
+}
+
+- (NSData *)cachedArtworkWithKey:(NSString *)key {
+	if([self hasCacheForKey:key] == YES) {
+		return [self getCachedData:key];
+	} else {
+		NSURL *dataUrl = [NSURL URLWithString:self.url];
+		NSData *data = [NSData dataWithContentsOfURL:dataUrl];
+		[self setCachedData:data forKey:key];
+		return data;
+	}
+}
+
+- (void)main {
 	NSLog(@"ArtworkLoader#main");
-	NSURL *artworkUrl = [NSURL URLWithString:self.url];
-	NSData *artworkData = [NSData dataWithContentsOfURL:artworkUrl];
+	NSData *artworkData = [self cachedArtworkWithKey:self.url];
 	UIImage *artworkImage = [UIImage imageWithData:artworkData];
 	[delegate loaderDidFinishLoadingArtwork:artworkImage fromURL:self.url];
 }
