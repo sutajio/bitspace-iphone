24df41b8a57074b3dd8a5a108d29b924d1b92e88
Niklas Holmgren <niklas.holmgren@mindset.se>
2010-07-15 23:23:08 +0200

    Changed the directory where cached tracks and artwork is stored to NSCachesDirectory.

--- ArtworkLoader.m
+++ ArtworkLoader.m
@@ -10,7 +10,7 @@
 #import "Release.h"
 
 
-#define cachePathForKey(key) [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:[NSString stringWithFormat:@"Cache/Artwork/%@", [key stringByReplacingOccurrencesOfString:@"/" withString:@"|"]]]
+#define cachePathForKey(key) [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:[NSString stringWithFormat:@"Artwork/%@", [key stringByReplacingOccurrencesOfString:@"/" withString:@"|"]]]
 
 
 @implementation ArtworkLoader
